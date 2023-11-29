c9d30a40d4167c5ce33b395d1a81b37cba474c30
Long <nguyhonglong2002@gmail.com>
2023-11-30 02:00:57 +0700

    upnew

--- backend/models/billModel.js
+++ backend/models/billModel.js
@@ -40,6 +40,10 @@ const billSchema = new mongoose.Schema({
         quantity: {
             type: Number,
             required: true
+        },
+        price:{
+            type: Number,
+            required: true
         }
     }]
 });
